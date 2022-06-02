40e656a3f26fd71f6947572c34802b58ab696c47
Sebastian Korinth <korinth@dipf.de>
2022-06-02 12:08:15 +0200

    added example for long to wide format conversion

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Beispiel Skript
 
-Im Unterordner "resultFolder" befinden sich Dummy-Dateien, mit zufällig generierten Ergebnissen. Diese 20 Dateien sollen in einem Loop zu einem R data frame vereint werden. 
+Im Unterordner "resultFolder" befinden sich Dummy-Dateien, mit zufällig generierten Ergebnissen. Diese 20 Dateien sollen in einem Loop zu einem R data frame vereint werden. In einem zweiten Schritt wird gezeigt, wie die so generierten Daten aus dem long ins wide Format umstrukturiert werden können.
 
 Zum Herunterladen des Skriptes und der Beispiel-Daten hier klicken:
 
--- mergeWithRexample.R
+++ mergeWithRexample.R
@@ -4,19 +4,16 @@
 
 # Im Unterordner "resultFolder" befinden sich Dummy-Dateien, mit zufällig 
 # generierten Ergebnissen. Dies 20 Dateien sollen mit folgendem Loop zu einem
-# R data frame vereint werden
-
+# R data frame vereint werden.
 
 # Erstellt eine Liste der relevanten Dateien. Wenn alle Dateien im Unterordner
 # "resultFolder" einbezogen werden sollen oder dort nur Dateien mit der Endung
 # ".csv" liegen, kann auf das Argument "pattern" verzichtet werden.
 file.list <- list.files(path = "resultFolder/", pattern = "*.csv")
 
-
 # Im folgenden Loop werden nacheinander alle Dateien eingelesen und über die
 # Funktion rbin() "übereinander gestapelt". Das Ergebnis ist ein data frame im
 # sogenannten long format namens "merged.data" (arbiträrer Name).
-
 for (file.counter in 1:length(file.list)) {
   temp.df <-
     read.csv(paste("resultFolder/", file.list[file.counter], sep = ""))
@@ -35,6 +32,39 @@ rm(temp.df, file.counter, file.list)
 
 # Speichern des data frames als Datei
 saveRDS(merged.data, file = "alleMeineDaten.Rds")
+
+
+###############################################################################
+############# Beispiel R-Skript: long zu wide Format ##########################
+###############################################################################
+
+# Obwohl die bevorzugte Datenstruktur in R das long-Format ist, könnte es nötig
+# werden, eine Umstrukturierung von Daten in das wide-Format vorzunehmen (z.B.,
+# wenn Kollegen lieber mit SPSS arbeiten). 
+# Hierfür gibt es unter R zahlreiche Optionen, von denen das reshape2-Paket für
+# lange Zeit als Standard galt. Da dieses Paket nicht mehr weiterentwickelt 
+# wird, empfehlen wir das Paket tidyr (vom selben Entwickler).
+
+library(tidyr)
+
+# Wir laden die Daten, welche im Skript "mergeWithRexample.R" erstellt wurden. 
+# Der data frame besteht aus 200 Zeilen und 4 Spalten
+long.format <- readRDS("alleMeineDaten.Rds")
+
+# Wollen wir jetzt zum Beispiel die Daten so umstrukturieren, dass jede Zeile 
+# eine Versuchsperson repräsentiert und jede Spalte die Reaktionszeit bzw. die
+# Reaktionsgenauigkeit für die einzelnen Trials, können wir die Funktion
+# "pivot_wider()" nutzen.
+
+wide.format <- pivot_wider(long.format, 
+                           names_from = trialIndex, 
+                           values_from = c(trialRT, trialCorrect))
+
+# Der resultierende data frame besteht aus 20 Zeilen (= 20 Versuchspersonen) 
+# und 21 Spalten (= 1 Spalte für die Versuchspersonennamen, 10 Spalten für die
+# Reaktionszeiten, 10 Spalten für Reaktionsgenauigkeit).
+# Weitere Informationen unter: https://tidyr.tidyverse.org
+
 
 
 
